91afff07ec56a26419bf2d0ce4ab1626bec6bad1
jakemarsdenjm <jakemarsdenjm@gmail.com>
2017-08-01 16:28:33 +1200

    Change colours on /index page based on age of food

--- fridgeometer/orm/models.py
+++ fridgeometer/orm/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from enum import Enum
 
 from fridgeometer import db
 
@@ -14,9 +15,25 @@ class FridgeEntry(db.Model):
     def storage_time(self) -> timedelta:
         return datetime.utcnow() - self.timestamp_stored
 
+    @property
+    def freshness(self) -> 'FreshnessState':
+        storage_time = self.storage_time
+        foodstuff = self.foodstuff
+        if storage_time > foodstuff.use_by:
+            return FreshnessState.Unusable
+        if storage_time > foodstuff.best_before:
+            return FreshnessState.NotFresh
+        return FreshnessState.Fresh
+
 
 class Foodstuff(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     best_before = db.Column(db.Interval, nullable=False)  # How long it takes for this food type to stop being fresh
     use_by = db.Column(db.Interval, nullable=False)  # How long it takes for this food type to stop being usable
+
+
+class FreshnessState(Enum):
+    Fresh = 1
+    NotFresh = 2
+    Unusable = 3
