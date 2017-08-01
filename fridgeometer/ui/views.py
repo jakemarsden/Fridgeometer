05a917b40d3a226867d60a944765793a1fa6079b
jakemarsdenjm <jakemarsdenjm@gmail.com>
2017-08-01 17:16:40 +1200

    Add delete button to fridge entries on /index page

--- fridgeometer/ui/views.py
+++ fridgeometer/ui/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import jsonify, render_template
 
-from fridgeometer import app
+from fridgeometer import app, db
 from fridgeometer.orm.models import FridgeEntry
 
 
@@ -11,3 +11,15 @@ def get_index():
                       .order_by(FridgeEntry.timestamp_stored)
                       .all())
     return render_template('index.html', fridge_entries=fridge_entries)
+
+
+@app.route('/fridge_entry/<int:entry_id>', methods=['DELETE'])
+def delete_entry(entry_id: int):
+    fridge_entry = (FridgeEntry.query
+                    .filter(FridgeEntry.id == entry_id)
+                    .one_or_none())
+    fridge_entry_exists = (fridge_entry is not None)
+    if fridge_entry_exists:
+        db.session.delete(fridge_entry)
+        db.session.commit()
+    return jsonify(success=fridge_entry_exists)
